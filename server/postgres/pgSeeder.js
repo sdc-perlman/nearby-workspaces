61f087466c0282864691001b8c0518f810671d6c
Collin <collinmcmahon2014@yahoo.com>
2021-03-18 00:58:57 -0400

    db implementations complete

--- server/postgres/pgSeeder.js
+++ server/postgres/pgSeeder.js
@@ -1,14 +1,13 @@
-const faker = require('faker');
 const sequelize = require('./');
 require('./relationship');
-const path = require('path');
-const uuid = require('uuid').v4;
+const generatePgData = require('./generatePgData');
 const fs = require('fs').promises;
 const argv = require('yargs').argv;
-const lines = argv.lines || 10;
+
+const lines = argv.lines || 1000000;
 const workspaceLocationsPath = './workspaceLocations.sql';
 const locationPointersPath = './locationPointers.sql'
-const batchSize = 10;
+const batchSize = 100000;
 
 sequelize.sync({ force: true })
   .then(() => {
@@ -17,29 +16,26 @@ sequelize.sync({ force: true })
       let workspaceLocations = ''
       let locationPointers = '';
       let batch = 0
-      fs.writeFile(workspaceLocationsPath, workspaceLocations);
-      fs.writeFile(locationPointersPath, locationPointers);
       for(let i = 1; i <= lines; i++) {
-        const streetNumber = faker.random.number(1000);
-        const streetName = faker.address.streetName();
-        const city = faker.address.city();
-        const state = faker.address.stateAbbr();
-        const zipCode = faker.address.zipCode().slice(0, 5);
-        const neighborhood = `${faker.address.direction()} ${city}`;
-        const rawAddress = `${streetNumber} ${streetName}, ${city}, ${state} ${zipCode}, USA`
-        const workspaceLocationsUuid = uuid();
-        workspaceLocations += `${workspaceLocationsUuid}|${faker.lorem.slug()}|${i}|${rawAddress}|${rawAddress}|${streetName}|${streetNumber}|${neighborhood}|${city}|${state}|United States|US|${zipCode}\n`
-        locationPointers += `${uuid()}|${i}|${faker.address.longitude(-117, -82)}|${faker.address.latitude(33, 41)}|${workspaceLocationsUuid}\n`;
+        const {
+          workspaceLocationsData,
+          locationPointersData,
+          rawQuery1,
+          rawQuery2
+        } = generatePgData(i);
+        workspaceLocations += workspaceLocationsData;
+        locationPointers += locationPointersData
         batch++;
         if(batch === batchSize) {
           try {
-            await fs.appendFile(workspaceLocationsPath, workspaceLocations);
-            await fs.appendFile(locationPointersPath, locationPointers);
-            await sequelize.query(`COPY public."WorkspaceLocations" ("uuid","workspaceSlug","workspaceId","rawAddress","formattedAddress","streetName","streetNumber","neighborhood","city","state","country","countryCode","zipCode") FROM '${path.join(__dirname, 'workspaceLocations.sql')}' WITH DELIMITER AS '|';`);
-            await sequelize.query(`COPY public."LocationPointers" ("uuid","workspaceId","longitude","latitude","workspaceLocation_id") FROM '${path.join(__dirname, 'locationPointers.sql')}' WITH DELIMITER AS '|';`);
-            await
+            await fs.writeFile(workspaceLocationsPath, workspaceLocations);
+            await fs.writeFile(locationPointersPath, locationPointers);
+            await sequelize.query(rawQuery1);
+            await sequelize.query(rawQuery2);
+            await fs.unlink(workspaceLocationsPath);
+            await fs.unlink(locationPointersPath);
           } catch(err) {
-            console.log('Error writing seed script file', err);
+            console.log('Error', err);
             process.exit();
           }
           workspaceLocations = '';
--- server/postgres/seed.sh
+++ server/postgres/seed.sh
@@ -8,11 +8,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # if parameter 1 is not passed as argument default records to be generated to 10 million rows for 2 tables
-LINES=${1:-10}
+LINES=${1:-10000000}
 
 psql -d postgres -f createDb.sql
 
 node pgSeeder.js --lines=$LINES
-
-# copy to db
-psql -d workspacelocations -f copyScript.sql
