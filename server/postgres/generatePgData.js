e3bb54d056b8a888653370a1501f2f8e1af5e64a
Collin <collinmcmahon2014@yahoo.com>
2021-03-18 01:14:46 -0400

    changed file type to csv from sql, improvment was small

--- server/postgres/generatePgData.js
+++ server/postgres/generatePgData.js
@@ -16,8 +16,8 @@ const generatePgData = (id) => {
   workspaceLocationsData += `${workspaceLocationsUuid}|${faker.lorem.slug()}|${id}|${rawAddress}|${rawAddress}|${streetName}|${streetNumber}|${neighborhood}|${city}|${state}|United States|US|${zipCode}\n`
   locationPointersData += `${uuid()}|${id}|${faker.address.longitude(-117, -82)}|${faker.address.latitude(33, 41)}|${workspaceLocationsUuid}\n`;
 
-  const rawQuery1 = `COPY public."WorkspaceLocations" ("uuid","workspaceSlug","workspaceId","rawAddress","formattedAddress","streetName","streetNumber","neighborhood","city","state","country","countryCode","zipCode") FROM '${path.join(__dirname, 'workspaceLocations.sql')}' WITH DELIMITER AS '|';`;
-  const rawQuery2 = `COPY public."LocationPointers" ("uuid","workspaceId","longitude","latitude","workspaceLocation_id") FROM '${path.join(__dirname, 'locationPointers.sql')}' WITH DELIMITER AS '|';`;
+  const rawQuery1 = `COPY public."WorkspaceLocations" ("uuid","workspaceSlug","workspaceId","rawAddress","formattedAddress","streetName","streetNumber","neighborhood","city","state","country","countryCode","zipCode") FROM '${path.join(__dirname, 'workspaceLocations.csv')}' WITH DELIMITER AS '|';`;
+  const rawQuery2 = `COPY public."LocationPointers" ("uuid","workspaceId","longitude","latitude","workspaceLocation_id") FROM '${path.join(__dirname, 'locationPointers.csv')}' WITH DELIMITER AS '|';`;
 
   return {
     workspaceLocationsData,
