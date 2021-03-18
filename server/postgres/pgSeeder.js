a1874164f101ea2bfb1cbc28534d76151198daae
Collin <collinmcmahon2014@yahoo.com>
2021-03-17 23:31:28 -0400

    couchdb setup and pg edits

--- server/postgres/pgSeeder.js
+++ server/postgres/pgSeeder.js
@@ -1,11 +1,14 @@
 const faker = require('faker');
 const sequelize = require('./');
 require('./relationship');
+const path = require('path');
 const uuid = require('uuid').v4;
 const fs = require('fs').promises;
 const argv = require('yargs').argv;
-const lines = argv.lines || 100;
-
+const lines = argv.lines || 10;
+const workspaceLocationsPath = './workspaceLocations.sql';
+const locationPointersPath = './locationPointers.sql'
+const batchSize = 10;
 
 sequelize.sync({ force: true })
   .then(() => {
@@ -13,16 +16,10 @@ sequelize.sync({ force: true })
     (async() => {
       let workspaceLocations = ''
       let locationPointers = '';
-      let previousFileIndex = 1;
-      let currentFileIndex = 1;
-      fs.writeFile(`./dataFiles/workspaceLocations${currentFileIndex}.sql`, workspaceLocations);
-      fs.writeFile(`./dataFiles/locationPointers${currentFileIndex}.sql`, locationPointers);
+      let batch = 0
+      fs.writeFile(workspaceLocationsPath, workspaceLocations);
+      fs.writeFile(locationPointersPath, locationPointers);
       for(let i = 1; i <= lines; i++) {
-        if(currentFileIndex > previousFileIndex) {
-          fs.writeFile(`./dataFiles/workspaceLocations${currentFileIndex}.sql`, workspaceLocations);
-          fs.writeFile(`./dataFiles/locationPointers${currentFileIndex}.sql`, locationPointers);
-          previousFileIndex = currentFileIndex;
-        }
         const streetNumber = faker.random.number(1000);
         const streetName = faker.address.streetName();
         const city = faker.address.city();
@@ -33,19 +30,21 @@ sequelize.sync({ force: true })
         const workspaceLocationsUuid = uuid();
         workspaceLocations += `${workspaceLocationsUuid}|${faker.lorem.slug()}|${i}|${rawAddress}|${rawAddress}|${streetName}|${streetNumber}|${neighborhood}|${city}|${state}|United States|US|${zipCode}\n`
         locationPointers += `${uuid()}|${i}|${faker.address.longitude(-117, -82)}|${faker.address.latitude(33, 41)}|${workspaceLocationsUuid}\n`;
-        try {
-          await fs.appendFile(`./dataFiles/workspaceLocations${currentFileIndex}.sql`, workspaceLocations);
-          await fs.appendFile(`./dataFiles/locationPointers${currentFileIndex}.sql`, locationPointers);
+        batch++;
+        if(batch === batchSize) {
+          try {
+            await fs.appendFile(workspaceLocationsPath, workspaceLocations);
+            await fs.appendFile(locationPointersPath, locationPointers);
+            await sequelize.query(`COPY public."WorkspaceLocations" ("uuid","workspaceSlug","workspaceId","rawAddress","formattedAddress","streetName","streetNumber","neighborhood","city","state","country","countryCode","zipCode") FROM '${path.join(__dirname, 'workspaceLocations.sql')}' WITH DELIMITER AS '|';`);
+            await sequelize.query(`COPY public."LocationPointers" ("uuid","workspaceId","longitude","latitude","workspaceLocation_id") FROM '${path.join(__dirname, 'locationPointers.sql')}' WITH DELIMITER AS '|';`);
+            await
+          } catch(err) {
+            console.log('Error writing seed script file', err);
+            process.exit();
+          }
           workspaceLocations = '';
           locationPointers = '';
-
-        } catch(err) {
-          console.log('Error writing seed script file', err);
-          process.exit();
-        }
-        //create a new file for every million
-        if(i.toString().slice(-6) === '000000') {
-          currentFileIndex++;
+          batch = 0;
         }
       }
       console.log('Seeding Complete');
